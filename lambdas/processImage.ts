129071775713130aa34c903e506c6662a6192cea
Zalán Tóth <zalant.tsc@gmail.com>
2025-11-25 18:21:02 +0000

    Started Fan out for S3 image upload.

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -2,8 +2,10 @@
 import { SQSHandler } from "aws-lambda";
 import {
   GetObjectCommand,
+  PutObjectCommandInput,
   GetObjectCommandInput,
   S3Client,
+  PutObjectCommand,
 } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client();
@@ -11,23 +13,25 @@ const s3 = new S3Client();
 export const handler: SQSHandler = async (event) => {
   console.log("Event ", JSON.stringify(event));
   for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);
-    if (recordBody.Records) {
-      console.log("Record body ", JSON.stringify(recordBody));
-      for (const messageRecord of recordBody.Records) {
-        const s3e = messageRecord.s3;
+    const recordBody = JSON.parse(record.body);        // Parse SQS message
+    const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
+
+    if (snsMessage.Records) {
+      console.log("Record body ", JSON.stringify(snsMessage));
+      for (const s3Message of snsMessage.Records) {
+        const s3e = s3Message.s3;
         const srcBucket = s3e.bucket.name;
         // Object key may have spaces or unicode non-ASCII characters.
         const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
         let theImage = null;
         try {
-          // Download the image from the S3                        bucket.
+          // Download the image from the S3 source bucket.
           const params: GetObjectCommandInput = {
             Bucket: srcBucket,
             Key: srcKey,
           };
           theImage = await s3.send(new GetObjectCommand(params));
-          // We can process the image here if required......
+          // Process the image ......
         } catch (error) {
           console.log(error);
         }
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -24,9 +24,14 @@ export class EDAAppStack extends cdk.Stack {
 
     // Integration infrastructure
 
-  const queue = new sqs.Queue(this, "img-uploadeded-q", {
-      receiveMessageWaitTime: cdk.Duration.seconds(5),
+  const imageProcessQueue = new sqs.Queue(this, "img-process-qq", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
+
+  const newImageTopic = new sns.Topic(this, "NewImageTopic", {
+      displayName: "New Image topic",
+    }); 
+
 
   // Lambda functions
 
@@ -41,14 +46,18 @@ export class EDAAppStack extends cdk.Stack {
       }
     );
 
-    // S3 --> SQS
+    // S3 --> SNS
     imagesBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
-      new s3n.SqsDestination(queue)
+      new s3n.SnsDestination(newImageTopic)
     );
 
-   // SQS --> Lambda
-    const newImageEventSource = new events.SqsEventSource(queue, {
+    newImageTopic.addSubscription(
+      new subs.SqsSubscription(imageProcessQueue)
+    );
+
+   // SNS --> Lambda
+    const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(5),
     });
